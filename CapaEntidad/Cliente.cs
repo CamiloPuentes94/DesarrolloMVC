0c20bd04fa6514a6f25ce9faf8a4747eb7bdd2e8
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-18 14:51:23 -0500

    Se realizan las entidades de la tabla de datos en la tabla Cliente

--- CapaEntidad/Cliente.cs
+++ CapaEntidad/Cliente.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    class Cliente
+
+    public class Cliente
     {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public bool Reestablecer { get; set; }
     }
 }
