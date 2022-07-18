fdd8dea6c36755334800e206a33b620b71039f42
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-18 14:48:37 -0500

    Se realizan las entidades de la tabla de datos en la tabla Productos

--- CapaEntidad/Producto.cs
+++ CapaEntidad/Producto.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    class Producto
+
+    public class Producto
     {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca oMarca { get; set; }
+        public Categoria oCategoria { get; set; }
+        public decimal precio { get; set; }
+        public int Stock { get; set; }
+        public string RutaImagen { get; set; }
+        public string NombreImagen { get; set; }
+        public bool Activo { get; set; }
     }
 }
