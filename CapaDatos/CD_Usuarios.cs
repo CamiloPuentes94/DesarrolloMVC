bfed8b3d2ff4e98e066ab9ef12216639d79ca933
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-19 15:38:14 -0500

    Se realiza logica de conexion de base de datos, consulta de base de datos y la lectura y almacenamiento de la base de datos en una lista

--- CapaDatos/CD_Usuarios.cs
+++ CapaDatos/CD_Usuarios.cs
@@ -4,11 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CapaDatos
 {
     public class CD_Usuarios
     {
-        public List<Usuario> Listar();
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                // realizando conexion y consulta sql
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select IdUsurio, Nombre, Apellido, Correo, Clave, Reestablecer,Activo from USUARIO";
+
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    //Lectrura de la base de datos y almacenada en la lista
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Usuario()
+                                {
+                                    IdUsuario = Convert.ToInt32(dr["IdUsurio"]),
+                                    Nombre = dr["Nombre"].ToString(),
+                                    Apellido = dr["Apellido"].ToString(),
+                                    Correo = dr["Correo"].ToString(),
+                                    Clave = dr["Clave"].ToString(),
+                                    Reestablecer = Convert.ToBoolean(dr["Reestablecer"]),
+                                    Activo = Convert.ToBoolean(dr["Activo"])
+                                });
+                        }
+                    }
+                }
+            }
+            // si ahi un error me genere una lista vacia
+            catch
+            {
+
+                lista = new List<Usuario>();
+            }
+
+            return lista;
+        }
+
+        
     }
 }
+ 
