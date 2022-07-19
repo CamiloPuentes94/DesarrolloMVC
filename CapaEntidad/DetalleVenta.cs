8885b8bf80511eb0a1ae18f168a2e7d58bfd55d4
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-19 14:41:56 -0500

    Se realiza referencia de clase de detalle de venta con venta como una lista

--- CapaEntidad/DetalleVenta.cs
+++ CapaEntidad/DetalleVenta.cs
@@ -8,12 +8,13 @@ namespace CapaEntidad
 {
     public class DetalleVenta
     {
-
         public int IdDetalleVenta { get; set; }
         public int IdVenta { get; set; }
         public Producto oProducto { get; set; }
         public int Cantidad { get; set; }
         public decimal Total { get; set; }
         public string IdTransaccion { get; set; }
+
+        public List<DetalleVenta> oDetalleVenta { get; set; }
     }
 }
