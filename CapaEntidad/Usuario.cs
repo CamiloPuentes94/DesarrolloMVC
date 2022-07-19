2ad0490e6d935f2e861d6e06df6846a2b88ee77d
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-19 14:47:15 -0500

    Se realiza estructura de la tabla Usuario

--- CapaEntidad/Usuario.cs
+++ CapaEntidad/Usuario.cs
@@ -8,5 +8,13 @@ namespace CapaEntidad
 {
     class Usuario
     {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public bool Reestablecer { get; set; }
+        public bool Activo { get; set; }
+
     }
 }
