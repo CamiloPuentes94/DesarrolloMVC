5a5ff9bf582e632f98717facb688461e10e2a130
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-19 14:30:10 -0500

    Se agrega metodo para la conexion de la base de datos'

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Configuration;
+
 namespace CapaDatos
 {
-    class Conexion
+    public class Conexion
     {
+        public static string cn = ConfigurationManager.ConnectionStrings["cadena"].ToString();
     }
 }
