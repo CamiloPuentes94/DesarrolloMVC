75897c2db5875e2f5f7b8eab23ce718b9b1bf98e
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-07-19 14:40:49 -0500

    Se quitan comentarios de la clase

--- CapaEntidad/Venta.cs
+++ CapaEntidad/Venta.cs
@@ -8,16 +8,6 @@ namespace CapaEntidad
 {
     public class Venta
     {
-        //        IdVenta int primary key identity,
-        //IdCliente int references CLIENTE(IdCliente),
-        //TotalProducto int,
-        //MonTotal decimal (10,2),
-        //Contacto varchar(50),
-        //IdUbicacion varchar(10),
-        //Telefono varchar(50),
-        //Direccion varchar(500),
-        //IdTransaccion varchar(50)
-
         public int IdVenta { get; set; }
         public int IdCliente { get; set; }
         public int TotalProducto { get; set; }
